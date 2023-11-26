993f55b2bc8a91659b1edda2a28a70a666f1a7e3
ArtmicRus <91434372+ArtmicRus@users.noreply.github.com>
2023-11-27 00:43:52 +0300

    Создал миграцию для заказов и добавил статусы заказа в админке (Кроме этого создал модель категорий товаров без миграции)

--- app/models.py
+++ app/models.py
@@ -44,7 +44,7 @@ class Comment(models.Model):
         return 'Комментарий %d %s к %s' % (self.id, self.author, self.post)
     # Метаданные вложенный класс который задает дополнительные параметры модели:
     class Meta:
-        db_table = "Comment"
+        db_table = "Comments"
         ordering = ["-date"]
         verbose_name = "Комментарии к статье блога"
         verbose_name_plural = "Комментарии к статьям блога"
@@ -52,12 +52,12 @@ class Comment(models.Model):
 admin.site.register(Comment)
 
 class Status(models.Model):
-    status_name = models.CharField(max_length=30, verbose_name="Название статуса")
+    name = models.CharField(max_length=30, verbose_name="Название статуса")
     
     def get_absolute_url(self):
         return reverse("statuses", args=[str(self.id)])
     def __str__(self):
-        return self.status_name
+        return self.name
 
     class Meta:
         db_table = "Statuses"# имя таблицы для модели
@@ -66,18 +66,37 @@ class Status(models.Model):
         
 admin.site.register(Status)
 
-# class Order(models.Model):
-#     user_id = models.ForeignKey(User, on_delete = models.CASCADE, verbose_name = "Покупатель")
-#     date = models.DateTimeField(default= datetime.now(), db_index=True, verbose_name= "Дата заказа")
-#     order_status = models.ForeignKey(Status, on_delete = models.CASCADE, verbose_name = "Статус заказа")
-#     total_price = models.FloatField(verbose_name = "Итоговая сумма заказа")
+class Order(models.Model):
+    user_id = models.ForeignKey(User, on_delete = models.CASCADE, verbose_name = "Покупатель")
+    date = models.DateTimeField(default= datetime.now(), db_index=True, verbose_name= "Дата заказа")
+    order_status = models.ForeignKey(Status, on_delete = models.CASCADE, verbose_name = "Статус заказа")
+    total_price = models.FloatField(verbose_name = "Итоговая сумма заказа")
     
-#     class Meta:
-#         db_table = "OrderStatuses"# имя таблицы для модели
-#         ordering = ["date"]
-#         verbose_name = "Статус заказа" 
-#         verbose_name_plural = "Статусы заказа" 
+    class Meta:
+        db_table = "Orderes"# имя таблицы для модели
+        ordering = ["date"]
+        verbose_name = "Заказ" 
+        verbose_name_plural = "Заказы" 
         
-# admin.site.register(Order)
+admin.site.register(Order)
 
+# class Product(models.Model):
+#     name = models.CharField(max_length=50, verbose_name="Название продукта")
+#     description = models.TextField(verbose_name="Краткое описание")
+#     content = models.TextField(verbose_name="Полное описание")
+#     price = models.FloatField(verbose_name = "Итоговая сумма заказа")
+#     image = models.FileField(default='temp.jpg', verbose_name = "Путь к картинке")
+    
+class Category(models.Model):
+    name = models.CharField(max_length=30, verbose_name="Название категории товаров")
+    
+    def get_absolute_url(self):
+        return reverse("сategories", args=[str(self.id)])
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "Categories"# имя таблицы для модели
+        verbose_name = "Категория товара" 
+        verbose_name_plural = "Категории товаров" 
 
